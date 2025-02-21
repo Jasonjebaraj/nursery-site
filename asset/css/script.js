c9cdf6ad6f6d7f59788e16dd122fb524e584ec2f
Jasonjebaraj <jasonjebaraj136@gmail.com>
2025-02-21 13:56:40 +0530

    jason jebaraj

---
+++ asset/css/script.js
@@ -0,0 +1,59 @@
+
+      
+$('.owl-carousel').owlCarousel({
+loop:false,
+margin:10,
+nav:true,
+responsive:{
+  0:{
+      items:1
+  },
+  600:{
+      items:1
+  },
+  1000:{
+      items:5
+  }
+}
+})
+
+
+
+
+$('.owl-carousel .owl-1').owlCarousel({
+    loop:false,
+    margin:10,
+    nav:true,
+    responsive:{
+      0:{
+          items:1
+      },
+      600:{
+          items:1
+      },
+      1000:{
+          items:1
+      }
+    }
+    })
+
+
+
+
+    $('.jas').owlCarousel({
+        loop:true,
+        margin:10,
+        nav:true,
+        responsive:{
+            0:{
+                items:1
+            },
+            600:{
+                items:3
+            },
+            1000:{
+                items:1
+            }
+        }
+    })
+
